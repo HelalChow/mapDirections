b10452ec2b491ab3d295d2062361a90d30518d8a
hchowdhury3273 <hc2324@nyu.edu>
2019-09-18 10:59:04 -0400

    added files

---
+++ mapRoute/mapRoute/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  mapRoute
+//
+//  Created by Helal Chowdhury on 9/17/19.
+//  Copyright © 2019 Helal. All rights reserved.
+//
+
+import UIKit
+impo
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
